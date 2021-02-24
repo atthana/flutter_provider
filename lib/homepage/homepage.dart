95ada53015361b477c0b893ea436b5fae3cdca93
atthana-p <atthana.p@codium.co>
2021-02-24 16:54:22 +0700

    4. เพิ่ม Provider.of<Counter>(context) มันเหมือนกับการสร้าง instance ขึ้นมา watch ตัว class Counter.

--- lib/homepage/homepage.dart
+++ lib/homepage/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:test_provider/homepage/counter.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -21,6 +23,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     print('==== build ====> $_counter');
+    final counter = Provider.of<Counter>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
